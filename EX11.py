9320b80a1c4de1bb6f8af2527980e737860a341c
Souhaïl Bechchari <146872818+SouhailBechchari@users.noreply.github.com>
2023-10-06 23:11:49 +0100

    Add files via upload

---
+++ EX11.py
@@ -0,0 +1,6 @@
+A = int(input("tapez le premier entier"))
+B = int(input("tapez le deuxieme entier"))
+if (A > 0 and B > 0) or (A < 0 and B < 0):
+    print("les deux nombres sont de meme signe")
+else : 
+    print("les deux nombres ne sont pas de meme signe")
